0934c37a552ed785eb35dd9b0756bb41a4e26dd8
Luca <lucamusio2752@gmail.com>
2026-03-30 21:37:37 +0200

    compress

--- huffman.c
+++ huffman.c
@@ -6,63 +6,202 @@
 /* --------------------- Compression --------------------- */
 
 // Da continuare
-void count_frequencies(FILE* fp, long* frequencies){
+void count_frequencies(FILE* fp, unsigned long* frequencies){
     fseek(fp, 0, SEEK_END);
     long size = ftell(fp);
 
     rewind(fp);
 
-    for(long i = 0; i < size; i++){
-        int byte = fgetc(fp);
+    int byte;
+    while((byte = fgetc(fp)) != EOF){
         frequencies[byte]++;
     }
+}
+
+void print_heap(MinHeap* heap){
+    for(int i = 0; i < heap->size; i++){
+        printf("Val: %c, Freq: %ld\n", heap->queue[i]->val, heap->queue[i]->freq);
+    }
+}
+
+void swap(Node **a, Node **b){
+    Node* temp = *a;
+
+    *a = *b;
+    *b = temp;
+
+}
+
+void heap_insert(MinHeap* heap, Node* node){
+    // Inserisco sempre in fondo
+    heap->queue[heap->size] = node;
+    unsigned long index = heap->size;
+    heap->size++;
+
+    while(index > 0 && 
+            heap->queue[(index - 1) / 2]->freq > 
+            heap->queue[index]->freq){
+        swap(&heap->queue[index], &heap->queue[(index - 1) / 2]);
+        index = (index - 1) / 2;
+    }
+
+}
+
+MinHeap* build_priority_queue(unsigned long* freq){
+    if(!freq) exit(1);   
+
+    MinHeap* heap = malloc(sizeof(MinHeap));
+    memset(heap, 0, sizeof(MinHeap));  
+
+    for(long i = 0; i < SIZE; i++){
+        if(freq[i] != 0){
+            Node* node = malloc(sizeof(Node));
+            
+            if(!node){
+                printf("Error: memory not allocated.\n");
+                exit(1);
+            }
+
+            node->val = i;
+            node->freq = freq[i];
+            node->left = NULL;
+            node->right = NULL;
+
+            heap_insert(heap, node);
+        }
+    }
+
+    return heap;
+}
+
+Node* heap_extract_min(MinHeap* heap){
+    if(!heap) exit(1);
     
-    return;
-}
-/*
-Queue build_priority_queue(unsigned int* freq){
-    if(!freq) return;        
+    Node* minNode = heap->queue[0];
+    heap->size--;
+    heap->queue[0] = heap->queue[heap->size];
 
-    return queue;
+    //Sift Down
+    int i = 0;
+    while(1){
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        int min = i; // assumiamo che il nodo corrente sia il minore
+
+        if(left < heap->size && 
+                heap->queue[left]->freq < heap->queue[min]->freq)
+            min = left;
+            
+        if(right < heap->size && 
+                heap->queue[right]->freq < heap->queue[min]->freq)
+            min = right;
+
+        if(min == i) break; // Nodo nel posto giusto
+
+        swap(&heap->queue[i], &heap->queue[min]);
+        i = min;        
+    }
+
+    return minNode;
 }
 
-Tree build_tree(){
+Node* build_tree(MinHeap* heap){
+    if(!heap) exit(1);
+
+    while(heap->size > 1){
+        // Estraggo due nodi
+        Node* left  = heap_extract_min(heap);
+        Node* right = heap_extract_min(heap);
+        
+        // Credo il loro padre
+        Node* root = malloc(sizeof(Node));
+        root->freq = left->freq + right->freq;
+        root->val = 0; //Nodo interno non ha carattere
+        root->left = left;
+        root->right = right;
+
+        // Lo inserisco nell'heap
+        heap_insert(heap, root);
+    }
+
+    // Ritorno il puntatore alla testa
+    return heap_extract_min(heap);
+}
+
+void generate_codes(Node* node, Code* table, unsigned char* buffer, int depth){
+    if(!node) return;
+
+    if(!node->left && !node->right){
+        memcpy(table[node->val].bits, buffer, (depth/8)+1);
+        table[node->val].length = depth;
+        return;
+    } 
     
-    return tree;
+    int byte_index = depth / 8;
+    int bit_index = depth % 8;
+    // Il bit più significativo viene prima
+    int pos = 7 - bit_index;
+    unsigned char mask = (unsigned char)(1 << pos);
+   
+    buffer[byte_index] |= mask;
+    generate_codes(node->left, table, buffer, depth + 1);
+    
+    buffer[byte_index] &= ~mask;
+    generate_codes(node->right, table, buffer, depth + 1);
 }
 
-void generate_codes(Tree* head){
-    if(!head) return;
+void write_compressed(char* filename_input){
+    // Creo il file
+    char filename_output[strlen(filename_input)];
+    strncpy(filename_output, filename_input, strlen(filename_input)-3); // 3 perchè devo togliere txt
+    strcat(filename_output, "huff");
+    printf("%s\n",filename_output);
 
-    return;
+    FILE *fp = fopen(filename_output, "w");
+    
+    // Generiamo l'Header: Numero di Foglie, Albero Serializzato, Numero totale di bit nel body
+    // Intestazione, scrivo HF
+    fwrite("HF", 1, 2, fp);
+    
+    
+
+    // Generiamo il Body: bit-code dei caratteri
+    
+
+    fclose(fp);
 }
 
-void write_compressed(){
-    return;
-}
-*/
 /* -------------------- Decompression -------------------- */
 
 
 /* --------------------- Functions ----------------------- */
 void huffman_compress(char* filename_input){
-    FILE* fp = fopen(filename_input, "r");
+    FILE* fp = fopen(filename_input, "rb");
+    printf("%s\n",filename_input);
 
     if(!fp){
         fprintf(stderr, "Error: file not found '%s'\n", filename_input);
         exit(1);
     }
 
-    long frequencies[SIZE];
+    unsigned long frequencies[SIZE];
     memset(frequencies, 0, sizeof(frequencies));
 
     count_frequencies(fp, frequencies);
-    /*
-    Queue queue = build_priority_queue();
-    Tree tree = build_tree();
-    generate_codes(&tree);
-    write_compressed();    
-    */
+    
+    MinHeap* heap = build_priority_queue(frequencies);
+    print_heap(heap);
+    
+    Node* root = build_tree(heap);
+    
+    Code table[SIZE];
+    memset(table, 0, sizeof(Code)*SIZE);
+    unsigned char buffer[32];
+    memset(buffer, 0, sizeof(buffer));
+    int depth = 0;
+    generate_codes(root, table, buffer, depth);
+    
+    write_compressed(filename_input);    
 
     fclose(fp);
 }
--- huffman.h
+++ huffman.h
@@ -3,6 +3,24 @@
 
 #define SIZE 256
 
+typedef struct Node{
+    unsigned char val;
+    unsigned long freq;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+typedef struct MinHeap{
+    Node* queue[SIZE]; // Al peggio ho 256 posti, in caso vado a ridurre   
+    int size; 
+} MinHeap;
+
+typedef struct Code{
+    // Al peggio devo usare 255 bit per una codifica, faccio quindi 32*8 = 256
+    unsigned char bits[32];
+    int length;
+} Code;
+
 void huffman_compress(char* filename_input);
 void huffman_decompress(char* filename_input);
 
